bec2bbe94c42b2aa09455d6af9eab960e8890605
Bob van de Vijver <bobv@XPS15-BOB>
2022-10-28 12:29:20 +0200

    Add renovate config + log

---
+++ renovate-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  autodiscover: false,
+  repositories: [
+    'bobvandevijver/renovate-18629'
+  ],
+  binarySource: 'docker',
+  platform: 'github',
+  token: 'redacted',
+  printConfig: true,
+  logFile: 'renovate-log.ndjson',
+  logFileLevel: 'debug',
+  allowPlugins: true,
+  allowScripts: true
+}
